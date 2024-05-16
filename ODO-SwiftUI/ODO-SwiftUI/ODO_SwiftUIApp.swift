552d1698d4b18673693a137af67af5fb1275aa92
DopeKS <kyusang.dev@gmail.com>
2024-05-16 18:50:05 +0900

    Add initial Xcode project for ODO-SwiftUI

---
+++ ODO-SwiftUI/ODO-SwiftUI/ODO_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ODO_SwiftUIApp.swift
+//  ODO-SwiftUI
+//
+//  Created by 황규상 on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct ODO_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
